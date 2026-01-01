1b32116e5e21968da990a4a0e836222e70e044e5
shailendr pawar <shailendrapawar980@gmail.com>
2026-01-01 18:14:45 +0530

    added:teams section

--- src/components/userCard/UserCard.jsx
+++ src/components/userCard/UserCard.jsx
@@ -4,14 +4,14 @@ const UserCard = ({ data }) => {
     const { theme } = useTheme()
     return (
         <main
-            className="h-40 w-40 flex flex-col gap-2 items-center justify-center bg-yellow-500 rounded-md"
+            className="h-40 w-40 flex flex-col gap-2 items-center justify-center bg-yellow-500 rounded-md hover:scale-98 transition-all ease-in-out"
             style={{
                 border: `1px solid ${theme.cardBorder}`,
                 backgroundColor: theme.cardBackground,
                 boxShadow: `2px 2px 5px ${theme.cardBorder}`
             }}
         >
-            <img className="h-20 w-20 bg-gray-500 object-cover rounded-full"
+            <img className="h-20 w-20 bg-gray-500 object-cover  rounded-full"
                 src={data?.img}
             ></img>
             <span className="text-center">
